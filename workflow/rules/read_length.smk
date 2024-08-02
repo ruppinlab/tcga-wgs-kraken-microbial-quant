3377d64e07cfc4d98525257f2657a77bfd565245
Leandro Hermida <softdev@leandrohermida.com>
2024-08-02 06:51:32 -0400

    BBMap reaadlength code updates and fixes

--- workflow/rules/read_length.smk
+++ workflow/rules/read_length.smk
@@ -5,12 +5,15 @@ rule bbmap_read_length_histogram:
             if wc.etype == "pe"
             else HOST_FILTERED_FASTQ_SE_FILE
         ),
+    params:
+        extra=config["bbmap"]["readlength"]["extra"],
     output:
         READ_LENGTH_HISTOGRAM_FILE,
     log:
         READ_LENGTH_HISTOGRAM_LOG,
+    threads: BBMAP_READLENGTH_THREADS
     wrapper:
-        READ_LENGTH_WRAPPER
+        BBMAP_READLENGTH_WRAPPER
 
 
 rule bbmap_max_read_length:
--- wrappers/bio/bbmap/readlength/wrapper.py
+++ wrappers/bio/bbmap/readlength/wrapper.py
@@ -6,7 +6,7 @@ import re
 
 from snakemake.shell import shell
 
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+log = snakemake.log_fmt_shell(stdout=True, stderr=True, append=True)
 
 fq1 = snakemake.input.get("fq1") or snakemake.input[0]
 assert fq1 is not None, "input: fq1 is a required named or positional input parameter"
@@ -17,7 +17,17 @@ if fq2:
 else:
     in2 = ""
 
-shellcmd = f"readlength.sh in='{fq1}' {in2} bin=1 out='{snakemake.output}' {log}"
+extra = snakemake.params.get("extra", "")
+
+shellcmd = (
+    f"JAVA_OPTS='-XX:ActiveProcessorCount={snakemake.threads}' readlength.sh"
+    f" in='{fq1}'"
+    f" {in2}"
+    f" bin=1"
+    f" out='{snakemake.output}'"
+    f" {extra}"
+    f" {log}"
+)
 shellcmd = re.sub(r"\s+", " ", shellcmd)
 with open(snakemake.log[0], "wt") as log_fh:
     log_fh.write(f"{shellcmd}\n")
