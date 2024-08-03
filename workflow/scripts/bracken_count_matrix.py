3eb977cd115376e8419645f9dec7c3a114aee4d1
Leandro Hermida <softdev@leandrohermida.com>
2024-08-03 16:37:34 -0400

    Fix count matrix issues

--- workflow/scripts/bracken_count_matrix.py
+++ workflow/scripts/bracken_count_matrix.py
@@ -7,15 +7,13 @@ assert sample_names is not None, "params: samples is a required parameter"
 
 count_matrix_df = pd.DataFrame()
 for count_file, sample_name in zip(count_files, sample_names):
-    count_df = pd.read_csv(
-        count_file, sep="\t", header=None, index_col=0, usecols=[0, 3]
-    )
+    count_df = pd.read_csv(count_file, sep="\t", header=0, index_col=0, usecols=[0, 3])
     count_df.columns = [sample_name]
     count_matrix_df = pd.concat(
         [count_matrix_df, count_df], axis=1, verify_integrity=True
     )
 
+count_matrix_df.fillna(0, inplace=True)
 count_matrix_df = count_matrix_df.astype(int)
-count_matrix_df.fillna(0)
 count_matrix_df.index.name = "Taxonomic name"
 count_matrix_df.to_csv(snakemake.output[0], sep="\t")
