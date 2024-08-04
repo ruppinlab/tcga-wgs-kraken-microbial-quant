bd25aab2b0fa9a5ca7acc50dc4677ef7876ba885
Leandro Hermida <softdev@leandrohermida.com>
2024-08-04 09:28:29 -0400

    Revert to defaults if not passed param

--- workflow/scripts/url_file.py
+++ workflow/scripts/url_file.py
@@ -5,9 +5,9 @@ from urllib.request import Request, urlopen
 
 req = Request(
     snakemake.params.url,
-    data=None,
-    headers=snakemake.params.headers,
-    method=snakemake.params.method,
+    data=snakemake.params.get("data"),
+    headers=snakemake.params.get("headers", {}),
+    method=snakemake.params.get("method"),
 )
 
 with urlopen(req) as res, open(snakemake.output[0], "wb") as out_file:
