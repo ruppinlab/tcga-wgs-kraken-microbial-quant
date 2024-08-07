e67638ed00c886288c47c434e20e287158a4f4fd
Leandro Hermida <softdev@leandrohermida.com>
2024-08-07 12:33:29 -0400

    Make things faster

--- workflow/scripts/kraken2_eupathdb_prot_library.py
+++ workflow/scripts/kraken2_eupathdb_prot_library.py
@@ -3,31 +3,32 @@ from contextlib import redirect_stdout, redirect_stderr
 
 import pandas as pd
 
+fasta_organism_regex = re.compile(
+    r"\s*\|\s*organism=(.+?)\s*\|\s*", flags=re.IGNORECASE
+)
 with open(snakemake.log[0], "wt") as log_fh:
     with redirect_stdout(log_fh), redirect_stderr(log_fh):
         with open(snakemake.output.idmap, "wt") as id_ofh:
             with open(snakemake.output.fasta, "wt") as fa_ofh:
                 with open(snakemake.input.fasta, "rt") as fa_ifh:
                     skipped_organisms = []
-                    meta_df = pd.read_csv(snakemake.input.meta, sep="\t")
+                    meta_df = pd.read_csv(
+                        snakemake.input.meta, sep="\t", index_col="Organism"
+                    )
                     for line in fa_ifh:
                         line = line.strip()
                         if line[0] == ">":
                             skip = False
-                            organism = re.findall(
-                                r"\s*\|\s*organism=(.+?)\s*\|\s*", line, re.IGNORECASE
-                            )
+                            organism = re.findall(fasta_organism_regex, line)
                             organism = organism[0].replace("_", " ")
-                            if organism not in meta_df["Organism"].values:
+                            if organism not in meta_df.index.values:
                                 skip = True
                                 if organism not in skipped_organisms:
                                     print(f"{organism} metadata not found, skipping")
                                     skipped_organisms.append(organism)
                             line_parts = re.split(r"\s*\|\s*", line)
                             seqid = line_parts[0].lstrip(">")
-                            taxid = meta_df.loc[
-                                meta_df["Organism"] == organism, "Species NCBI taxon ID"
-                            ].squeeze()
+                            taxid = meta_df.loc[organism, "Species NCBI taxon ID"]
                             id_ofh.write(
                                 f"TAXID\tkraken:taxid|{taxid}|{seqid}\t{taxid}\n"
                             )
