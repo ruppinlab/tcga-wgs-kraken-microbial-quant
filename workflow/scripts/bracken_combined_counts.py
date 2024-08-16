0f8d547f82b842f6601639b70ffff4b108bbc9fb
Leandro Hermida <softdev@leandrohermida.com>
2024-08-16 10:12:10 -0400

    Fix dataframe index_col and usecols handling

--- workflow/scripts/bracken_combined_counts.py
+++ workflow/scripts/bracken_combined_counts.py
@@ -6,15 +6,14 @@ use_cols = [
     "added_reads",
     "new_est_reads",
 ]
-idx_col_name = "taxonomy_id"
 
 count_sum_df = pd.DataFrame()
 for count_file in snakemake.input:
     count_df = pd.read_csv(
-        count_file, sep="\t", header=0, index_col=idx_col_name, usecols=use_cols
-    )[use_cols]
+        count_file, sep="\t", header=0, index_col=use_cols[0], usecols=use_cols
+    )[use_cols[1:]]
     count_sum_df = pd.concat([count_sum_df, count_df], axis=0).groupby(level=0).sum()
 
-count_sum_df.index.name = idx_col_name
+count_sum_df.index.name = use_cols[0]
 count_sum_df = count_sum_df.astype(int)
 count_sum_df.to_csv(snakemake.output[0], sep="\t")
--- workflow/scripts/bracken_count_matrix.py
+++ workflow/scripts/bracken_count_matrix.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 use_cols = ["taxonomy_id", "new_est_reads"]
-idx_col_name = "taxonomy_id"
 
 count_files = snakemake.input.get("counts") or snakemake.input
 assert count_files is not None, "input: counts is a required parameter"
@@ -11,14 +10,14 @@ assert sample_names is not None, "params: samples is a required parameter"
 count_matrix_df = pd.DataFrame()
 for count_file, sample_name in zip(count_files, sample_names):
     count_df = pd.read_csv(
-        count_file, sep="\t", header=0, index_col=idx_col_name, usecols=use_cols
-    )[use_cols]
+        count_file, sep="\t", header=0, index_col=use_cols[0], usecols=use_cols
+    )[use_cols[1:]]
     count_df.columns = [sample_name]
     count_matrix_df = pd.concat(
         [count_matrix_df, count_df], axis=1, verify_integrity=True
     )
 
+count_matrix_df.index.name = use_cols[0]
 count_matrix_df.fillna(0, inplace=True)
 count_matrix_df = count_matrix_df.astype(int)
-count_matrix_df.index.name = idx_col_name
 count_matrix_df.to_csv(snakemake.output[0], sep="\t")
