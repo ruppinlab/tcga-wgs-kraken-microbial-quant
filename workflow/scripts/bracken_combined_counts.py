1e032becdca4f6c852c009a046cfcbb9b6d3ec1c
Leandro Hermida <softdev@leandrohermida.com>
2024-08-23 14:03:26 -0400

    Update Bracken file combining and matrix to include feature metadata

--- workflow/scripts/bracken_combined_counts.py
+++ workflow/scripts/bracken_combined_counts.py
@@ -1,21 +1,16 @@
 import pandas as pd
 
-use_cols = [
-    "taxonomy_id",
-    "kraken_assigned_reads",
-    "added_reads",
-    "new_est_reads",
-]
+idx_cols = ["name", "taxonomy_id", "taxonomy_lvl"]
 
 all_count_df = pd.DataFrame()
 for count_file in snakemake.input:
-    count_df = pd.read_csv(
-        count_file, sep="\t", header=0, index_col=use_cols[0], usecols=use_cols
-    )[use_cols[1:]]
+    count_df = pd.read_csv(count_file, sep="\t", header=0, index_col=idx_cols)
+    count_df.drop(columns=["fraction_total_reads"], inplace=True)
     if not count_df.empty:
         all_count_df = pd.concat([all_count_df, count_df], axis=0)
 
-count_sum_df = all_count_df.groupby(level=0).sum()
-count_sum_df.index.name = use_cols[0]
-count_sum_df = count_sum_df.astype(int)
+count_sum_df = all_count_df.groupby(idx_cols).sum()
+count_sum_df["fraction_total_reads"] = round(
+    count_sum_df["new_est_reads"] / count_sum_df["new_est_reads"].sum(), 5
+)
 count_sum_df.to_csv(snakemake.output[0], sep="\t")
--- workflow/scripts/bracken_count_matrix.py
+++ workflow/scripts/bracken_count_matrix.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-use_cols = ["taxonomy_id", "new_est_reads"]
+idx_cols = ["name", "taxonomy_id", "taxonomy_lvl"]
+use_cols = idx_cols + ["new_est_reads"]
 
 count_files = snakemake.input.get("counts") or snakemake.input
 assert count_files is not None, "input: counts is a required parameter"
@@ -10,14 +11,13 @@ assert sample_names is not None, "params: samples is a required parameter"
 count_matrix_df = pd.DataFrame()
 for count_file, sample_name in zip(count_files, sample_names):
     count_df = pd.read_csv(
-        count_file, sep="\t", header=0, index_col=use_cols[0], usecols=use_cols
-    )[use_cols[1:]]
+        count_file, sep="\t", header=0, index_col=idx_cols, usecols=use_cols
+    )
     count_df.columns = [sample_name]
     count_matrix_df = pd.concat(
         [count_matrix_df, count_df], axis=1, verify_integrity=True
     )
 
-count_matrix_df.index.name = use_cols[0]
 count_matrix_df = count_matrix_df.infer_objects().fillna(0)
 count_matrix_df = count_matrix_df.astype(int)
 count_matrix_df.sort_index(inplace=True)
