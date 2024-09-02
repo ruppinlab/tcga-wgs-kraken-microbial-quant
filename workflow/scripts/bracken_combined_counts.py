7890e3250a4f95d4a27babd74c9b5282e79763ea
Leandro Hermida <softdev@leandrohermida.com>
2024-09-02 17:57:24 -0400

    Merge all dfs at once

--- workflow/scripts/bracken_combined_counts.py
+++ workflow/scripts/bracken_combined_counts.py
@@ -2,13 +2,14 @@ import pandas as pd
 
 idx_cols = ["name", "taxonomy_id", "taxonomy_lvl"]
 
-all_count_df = pd.DataFrame()
+count_dfs = []
 for count_file in snakemake.input:
     count_df = pd.read_csv(count_file, sep="\t", header=0, index_col=idx_cols)
     count_df.drop(columns=["fraction_total_reads"], inplace=True)
     if not count_df.empty:
-        all_count_df = pd.concat([all_count_df, count_df], axis=0)
+        count_dfs.append(count_df)
 
+all_count_df = pd.concat(count_dfs, axis=0)
 count_sum_df = all_count_df.groupby(idx_cols).sum()
 count_sum_df["fraction_total_reads"] = round(
     count_sum_df["new_est_reads"] / count_sum_df["new_est_reads"].sum(), 5
--- workflow/scripts/bracken_count_matrix.py
+++ workflow/scripts/bracken_count_matrix.py
@@ -8,16 +8,15 @@ assert count_files is not None, "input: counts is a required parameter"
 sample_names = snakemake.params.get("samples")
 assert sample_names is not None, "params: samples is a required parameter"
 
-count_matrix_df = pd.DataFrame()
+count_dfs = []
 for count_file, sample_name in zip(count_files, sample_names):
     count_df = pd.read_csv(
         count_file, sep="\t", header=0, index_col=idx_cols, usecols=use_cols
     )
     count_df.columns = [sample_name]
-    count_matrix_df = pd.concat(
-        [count_matrix_df, count_df], axis=1, verify_integrity=True
-    )
+    count_dfs.append(count_df)
 
+count_matrix_df = pd.concat(count_dfs, axis=1, verify_integrity=True)
 count_matrix_df = count_matrix_df.infer_objects().fillna(0)
 count_matrix_df = count_matrix_df.astype(int)
 count_matrix_df.sort_index(inplace=True)
