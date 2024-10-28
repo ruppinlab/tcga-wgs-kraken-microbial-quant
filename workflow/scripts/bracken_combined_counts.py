a76eba9b740ab4d9d332fd64598ffe61f1bd453e
Leandro Hermida <softdev@leandrohermida.com>
2024-10-28 03:40:33 -0400

    Fix np pair special handling logic

--- workflow/scripts/bracken_combined_counts.py
+++ workflow/scripts/bracken_combined_counts.py
@@ -2,22 +2,31 @@ import pandas as pd
 
 idx_cols = ["name", "taxonomy_id", "taxonomy_lvl"]
 
-np_pair = snakemake.params.get("np_pair", False)
-if np_pair and len(snakemake.input) != 2:
-    raise ValueError(
-        "Input must be a nucl and prot pair of count files when --np-pair is set"
-    )
-
 count_dfs = []
-for count_file in snakemake.input:
-    count_df = pd.read_csv(count_file, sep="\t", header=0, index_col=idx_cols)
-    if not count_df.empty:
-        count_df.drop(columns=["fraction_total_reads"], inplace=True)
-        count_dfs.append(count_df)
+np_pair = snakemake.params.get("np_pair", False)
+
+if np_pair:
+    if len(snakemake.input) != 2:
+        raise ValueError(
+            "Input must be a nucl and prot pair of count files when --np-pair is set"
+        )
+    nucl_df = pd.read_csv(snakemake.input[0], sep="\t", header=0, index_col=idx_cols)
+    prot_df = pd.read_csv(snakemake.input[1], sep="\t", header=0, index_col=idx_cols)
+    if not nucl_df.empty:
+        nucl_df.drop(columns=["fraction_total_reads"], inplace=True)
+        count_dfs.append(nucl_df)
+        prot_df = prot_df.loc[prot_df.index.isin(nucl_df.index)]
+        if not prot_df.empty:
+            prot_df.drop(columns=["fraction_total_reads"], inplace=True)
+            count_dfs.append(prot_df)
+else:
+    for count_file in snakemake.input:
+        count_df = pd.read_csv(count_file, sep="\t", header=0, index_col=idx_cols)
+        if not count_df.empty:
+            count_df.drop(columns=["fraction_total_reads"], inplace=True)
+            count_dfs.append(count_df)
 
 if count_dfs:
-    if np_pair and len(count_dfs) == 2:
-        count_dfs[1] = count_dfs[1].loc[count_dfs[1].index.isin(count_dfs[0].index)]
     all_count_df = pd.concat(count_dfs, axis=0)
     count_sum_df = all_count_df.groupby(idx_cols).sum()
     count_sum_df["fraction_total_reads"] = round(
